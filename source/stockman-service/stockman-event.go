06d6825c2a46782d51a496155dedf6b623db6d7c
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-08 14:15:12 +0300

    added Done and StopEvent methods

--- source/stockman-service/stockman-event.go
+++ source/stockman-service/stockman-event.go
@@ -14,9 +14,19 @@ type Event[T any, K any, B any] struct {
 	Output          chan<- T // Output data channel to get actual data from process
 	Input           <-chan K // Input data channel to send actual data inside process
 	Data            B        // First data to send with event just
+	ctx             context.Context
+	cancelFunc      context.CancelFunc
 }
 
 func (e *Event[T, K, B]) Run() {}
+
+func (e *Event[T, K, B]) Done() <-chan struct{} {
+	return e.ctx.Done()
+}
+
+func (e *Event[T, K, B]) StopEvent() {
+	e.cancelFunc()
+}
 
 func NewEvent[T any, K any, B any]() *Event[T, K, B] {
 	return &Event[T, K, B]{}
