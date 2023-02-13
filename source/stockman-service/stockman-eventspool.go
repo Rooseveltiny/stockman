638aa9f1cc989adb361c0315d42aa1f69359755a
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-13 16:11:11 +0300

    added events pool

--- source/stockman-service/stockman-eventspool.go
+++ source/stockman-service/stockman-eventspool.go
@@ -1,1 +1,22 @@
 package main
+
+type EventsPool struct {
+	Events *EventsList
+}
+
+func (ep *EventsPool) Size() int {
+	return ep.Events.Size()
+}
+
+func (ep *EventsPool) AppendEvent(event *Event) {
+	ep.Events.AppendEvent(event)
+}
+
+func (ep *EventsPool) PopEvent() *Event {
+	return ep.Events.PopEvent()
+}
+
+func NewEventsPool() *EventsPool {
+	eventsList := NewEventsList()
+	return &EventsPool{Events: eventsList}
+}
