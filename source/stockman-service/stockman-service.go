35062be780ed2f6ffd03507e88dbb07c09e8983c
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-15 20:50:23 +0300

    added stockman service

--- source/stockman-service/stockman-service.go
+++ source/stockman-service/stockman-service.go
@@ -1,1 +1,17 @@
 package main
+
+/*
+Core entity to start stockman system. Here is going to be loaded stockman service and all
+other subsystems i.e. stockman rest API backend or DB client.
+*/
+type StockmanService struct {
+	SystemEventsManager *SystemEventsManager
+}
+
+func (ss *StockmanService) StartSystem() {
+
+}
+
+func NewStockmanService() *StockmanService {
+	return &StockmanService{}
+}
