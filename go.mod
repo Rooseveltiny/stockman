46807399b35a0931b6db2ae3af1ec560460e9801
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-16 23:58:13 +0300

    adding come new db dependencies

--- go.mod
+++ go.mod
@@ -4,11 +4,22 @@ go 1.20
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jackc/pgconn v1.14.0
+	github.com/jackc/pgx/v4 v4.18.0
 	github.com/smartystreets/goconvey v1.7.2
 )
 
 require (
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
---
+++ source/stockman_dbs/client/postgresql/client.go
@@ -0,0 +1,34 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"restapi-lesson/internal/config"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+func NewClient(ctx context.Context, maxAttempts int, sc config.StorageConfig) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", sc.Username, sc.Password, sc.Host, sc.Port, sc.Database)
+
+	pool, err = pgxpool.Connect(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err != nil {
+		log.Fatal("can't connect to db")
+	}
+
+	return pool, nil
+}
