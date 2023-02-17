16d28576a2adff7b3d19d84943c4d77dd7d36e25
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-17 22:52:23 +0300

    y

--- source/stockman_logger/logger.go
+++ source/stockman_logger/logger.go
@@ -16,8 +16,8 @@ Special place to work with logging subsystem
 var L *logrus.Logger
 
 func InitLogger() {
-	// init logger instance to use it across an application
 
+	// init logger instance to use it across an application
 	f, err := os.OpenFile("system_logging.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println("Failed to create logfile" + "system_logging.log")
@@ -32,6 +32,7 @@ func InitLogger() {
 			LogFormat:       "[%lvl%]: %time% - %msg%\n",
 		},
 	}
+
 }
 
 func init() {
