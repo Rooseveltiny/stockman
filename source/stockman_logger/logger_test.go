d18ba79ce4dc2de9985d4d970fe598e4ed3f1523
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-17 22:44:43 +0300

    added test for logging system

--- source/stockman_logger/logger_test.go
+++ source/stockman_logger/logger_test.go
@@ -1,6 +1,9 @@
 package logger
 
 import (
+	"fmt"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/smartystreets/goconvey/convey"
@@ -9,6 +12,13 @@ import (
 func TestLogger(t *testing.T) {
 
 	convey.Convey("try to log smth", t, func() {
+		f, err := os.OpenFile("test_logging.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println("Failed to create logfile" + "system_logging.log")
+			panic(err)
+		}
+		defer f.Close()
+		L.Out = io.MultiWriter(f, os.Stdout)
 		L.Info("this is info message")
 	})
 
