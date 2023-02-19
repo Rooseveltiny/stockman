c7dcc9f176bd5d24ce715ede36563f2ab96d36c7
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-19 16:57:29 +0300

    added test for init db postgres connection; added postgres utils package

--- source/stockman_dbs/client/postgresql/client_test.go
+++ source/stockman_dbs/client/postgresql/client_test.go
@@ -1,6 +1,8 @@
 package postgresql
 
 import (
+	"context"
+	logger "stockman/source/stockman_logger"
 	"testing"
 
 	"github.com/smartystreets/goconvey/convey"
@@ -10,5 +12,16 @@ func TestDBConfigInit(t *testing.T) {
 	convey.Convey("test init db settings", t, func() {
 		db_c := NewPostgresConfig()
 		convey.So(db_c.Host, convey.ShouldEqual, "localhost")
+	})
+}
+
+func TestPostgresClientInit(t *testing.T) {
+	convey.Convey("test postgres client init process", t, func() {
+		db_c := NewPostgresConfig()
+		c, err := NewClient(context.TODO(), *db_c)
+		if err != nil {
+			logger.L.Errorln(err)
+		}
+		convey.So(c, convey.ShouldNotBeNil)
 	})
 }
