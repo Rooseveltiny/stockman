4dc1628779c286daa26a1088234733e42bf5f5b0
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-19 17:01:49 +0300

    starting to add some postgres utils

--- source/stockman_dbs/postgres_utils/utils.go
+++ source/stockman_dbs/postgres_utils/utils.go
@@ -1,1 +1,10 @@
 package postgresutils
+
+import (
+	"context"
+	"stockman/source/stockman_dbs/client/postgresql"
+)
+
+func RunSQLFile(ctx context.Context, client postgresql.Client, filePath string) {}
+
+func RunSQLFiles(ctx context.Context, client postgresql.Client, filePath []string) {}
