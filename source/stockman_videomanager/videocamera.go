8f65a013359de4be18069b764505f4cf4e4fe2a5
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2023-02-23 16:33:45 +0300

    adding videocamera entity

---
+++ source/stockman_videomanager/videocamera.go
@@ -0,0 +1,18 @@
+package stockmanvideomanager
+
+import videocamera "stockman/source/stockman_dbs/data_models/video_camera"
+
+type VideoCamera struct {
+	Address  string
+	Port     string
+	Login    string
+	Password string
+	Link     string
+}
+
+func (vc *VideoCamera) CheckConnectionStatus() bool { return false }
+func (vc *VideoCamera) VideoStream()                {}
+
+func CreateNewCamera(camera videocamera.CameraCreateDTO) *VideoCamera { return &VideoCamera{} }
+func AllVideoCameras() []*VideoCamera                                 { return nil }
+func UpdateVideoCamera()
